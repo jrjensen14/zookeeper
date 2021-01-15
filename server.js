b12f8dc91fa02bff0e0ea1a32b2f0463c160d968
JRJ <jjsocc171@msn.com>
2021-01-15 10:03:44 -0600

    connected front-end & back-end data

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
-const { runInNewContext } = require('vm');
-const PORT = process.env.PORT || 3001;
-const app = express();
+// const { runInNewContext } = require('vm');
 const fs = require('fs');
 const path = require('path');
+const { animals } = require('./data/animals.json');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
 
 //parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
-const { animals } = require('./data/animals.json');
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -113,11 +116,19 @@ app.post('/api/animals', (req, res) => {
         // console.log(req.body);
         res.json(animal);
     }
-
-
-
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'))
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on ${PORT}!`);
